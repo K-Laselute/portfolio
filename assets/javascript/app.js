2188d45186b0641bc8ff548816c0bd6c6c2ce314
Kyle Laselute <klaselute@gmail.com>
2019-08-06 22:44:44 -0600

    new image added. Added Html, styling, and script.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
 const mainContent = document.querySelector('#main');
+const navLinks = document.querySelectorAll('nav a');
 
 mainContent.style.display = "none";
+
+$(navLinks).click(function() {
+    
+});
