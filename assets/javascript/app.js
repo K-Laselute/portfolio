849ae38cb2715a3b810e333bd33fcc52b6805792
Kyle Laselute <klaselute@gmail.com>
2019-08-09 16:22:45 -0600

    Applying different layout removed js

--- assets/javascript/app.js
+++
@@ -1,8 +0,0 @@
-const mainContent = document.querySelector('#main');
-const navLinks = document.querySelectorAll('nav a');
-
-mainContent.style.display = "none";
-
-$(navLinks).click(function() {
-    
-});
