34ef212b9b6d13756aaf1308742c835740be5c52
Kyle Laselute <klaselute@gmail.com>
2019-08-13 15:03:20 -0600

    Added jquery animation for links.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,29 @@
-let $work = $('#work');
-let $skills = $('#skills');
-let $about = $('#about');
-let $contact = $('#contact');
-
-$($work).hide();
-$($skills).hide();
-$($about).hide();
-$($contact).hide();
-
+let $mainContainer = $('.main-container');
+let $main = $('#main');
+let $mainArticles = $main.children();
 let $navLinks = $('nav li a');
+
+$mainArticles.hide();
 
 $($navLinks).on('click', function () {
     if($(this).text() === 'Work') {
-        $work.show("slow");
-        $('.main-container').hide();
+        $('#work').show("slow");
+        $('.main-container').slideUp();
+        $('footer').hide();
+    }
+    if($(this).text() === 'Skills') {
+        $('#skills').show("slow");
+        $('.main-container').slideUp();
+        $('footer').hide();
+    }
+    if($(this).text() === 'About') {
+        $('#about').show("slow");
+        $('.main-container').slideUp();
+        $('footer').hide();
+    }
+    if($(this).text() === 'Contact') {
+        $('#contact').show("slow");
+        $('.main-container').slideUp();
+        $('footer').hide();
     }
 });
