fb2f02e3d12f0e69a7b65650007c9823e4c6e9ae
Kyle Laselute <klaselute@gmail.com>
2019-08-13 15:49:04 -0600

    Added jquery scrip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,18 @@ let $navLinks = $('nav li a');
 
 $mainArticles.hide();
 
+$(document).mouseup(function(e) 
+{
+    var container = $($mainArticles);
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        container.hide();
+        $mainContainer.fadeIn();
+    }
+});
+
 $($navLinks).on('click', function () {
     if($(this).text() === 'Work') {
         $('#work').show("slow");
